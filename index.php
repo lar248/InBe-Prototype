cb6afc183c3c71c2bba3e0617c56d8802476156a
Your Name HereLarry <LaurenceRosenzweig@dhcp-gs-5570.redrover.cornell.edu>
2014-10-22 14:08:51 -0400

    Adding New Files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>InBe</title>
+        <link type="text/css" rel="stylesheet" href="styles/styles.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+            
+    </head>
+    
+    <body>
+        <div id="wrapper">
+            <div id="top">
+                <img id="menuButton" src="Images/Menu_Button.png" alt="menu" width="50px">
+                <img id="cameraButton" src="Images/Camera_Button.png" alt="menu" width="50px">
+            </div>
+            
+        </div>
+    </body>
+    
+    <script>
+        var x = .3 - .2;
+        var y = .2 - .1;
+        
+        var s = "hello, world";
+        
+        
+    </script>
+
+</html>
