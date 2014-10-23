38bda99f4900ce2ea041ae1291fcc469c588a7e6
Your Name HereLarry <LaurenceRosenzweig@Laurences-MacBook-Pro.local>
2014-10-22 22:32:49 -0400

    scrolling, camera working

--- index.php
+++ index.php
@@ -14,17 +14,64 @@
     </head>
     
     <body>
-        <div data-role="page" id="wrapper">
+        <div data-role="page" id="home">
+            <header data-role="header" id="top">
+                <h1>InBe</h1>
+                <a href="#myPanel"><img id="menuButton" src="Images/Menu_Button.png" alt="menu" width="50px"></a>
+                <a href="#camera" data-transition="slide"><img id="cameraButton" src="Images/Camera_Button.png" alt="menu" width="50px"></a>
+            </header>
+            
+            
+            <div data-role="listview" class="ui-grid-a" data-filter="true">
+                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#"><img id="img1" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>
+                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#"><img id="img2" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>
+                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#"><img id="img3" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>
+                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#"><img id="img4" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>
+                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#"><img id="img5" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>
+                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#"><img id="img6" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>
+                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#"><img id="img7" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>
+                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#"><img id="img8" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>   
+            </div><!-- /grid-a -->
+            
+            <div data-role="panel" id="myPanel" data-theme="b" data-display="push">
+                <a href="" data-role="button" data-rel="close">Close Me</a>
+                <p>this is a thing thing hting fv </p>
+            </div>
+        </div>
+        
+        <div data-role="page" id="camera">
+            <div data-role="header">
+                <h1>Page 2</h1>
+              </div>
+              <div role="main" class="ui-content">
+                <a href="#home" data-rel="back" data-direction: "reverse" data-transition="slide" class="ui-btn ui-corner-all ui-btn-inline">Go Back To Page 1</a>
+              </div>
+            <video id="video" width="640" height="480" autoplay></video>
+            <button id="snap">Snap Photo</button>
+            <canvas id="canvas" width="640" height="480"></canvas>
+        </div>
+        
+        
+        
+        <!--
+        <div data-role="page" id="homepage">
             <div data-role="header" id="top">
                 <a href="#myPanel"><img id="menuButton" src="Images/Menu_Button.png" alt="menu" width="50px"></a>
                 <a href="#myPanel2"><img id="cameraButton" src="Images/Camera_Button.png" alt="menu" width="50px"></a>
             </div>
             
             <div data-role="content" class="ui-grid-a">
-                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px">Image A goes here</div></div>
-                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px">Image B goes here</div></div>
+                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px"><a href="#img1"><img id="img1" src="Images/losangelos.jpg" alt="menu" width="100%"></a></div></div>
+                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px"><img id="img2" src="Images/losangelos.jpg" alt="menu" width="100%"></div></div>
+                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px"><img id="img3" src="Images/losangelos.jpg" alt="menu" width="100%"></div></div>
+                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px"><img id="img4" src="Images/losangelos.jpg" alt="menu" width="100%"></div></div>
+                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px"><img id="img5" src="Images/losangelos.jpg" alt="menu" width="100%"></div></div>
+                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px"><img id="img6" src="Images/losangelos.jpg" alt="menu" width="100%"></div></div>
+                <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:200px"><img id="img7" src="Images/losangelos.jpg" alt="menu" width="100%"></div></div>
+                <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:200px"><img id="img8" src="Images/losangelos.jpg" alt="menu" width="100%"></div></div>
             </div><!-- /grid-a -->
-            
+        -->
+        <!--
             <div data-role="panel" id="myPanel" data-theme="b" data-display="push">
                 <a href="" data-role="button" data-rel="close">Close Me</a>
                 <p>this is a thing thing hting fv </p>
@@ -35,10 +82,27 @@
                 <p>this is a thing thing hting fv </p>
             </div>
             
+            <div data-role="page" id="img1">
+                <div data-role="header">
+                    <h3>
+                        InBe
+                    </h3>
+                </div>
+                <div data-role="content">
+                    <h3>
+                        InBe
+                    </h3>
+                    <p>
+                        blah blah shit blah blah shit  blah blah shit  blah blah shit  blah blah shit  blah blah shit 
+                    </p>
+                </div>
+            </div>
+            
             <div id="result">
                 
             </div>
             
         </div>
+        -->
     </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -1,13 +1,63 @@
 var hash = {};
 
 $(document).ready(function() {
-    $(document).on("swipeleft", function(event, ui) {
-        $("#myPanel2").panel("open");
-    });
-    $(document).on("swiperight", function(event, ui) {
+    init();
+    $(this).on("swiperight", function(event, ui) {
         $("#myPanel").panel("open");
     });
-    init();
+    
+    
+    //Not working yet
+    $("img").each(function(index) {
+        var elementId = $(this).attr("id");
+        elementId = "#" + elementId;
+        $(function () {
+            $(elementId).on("click", function(event) {
+                $.mobile.changePage(elementId, {transition: "slideup"});
+            });
+            
+        });
+    });
+    
+    // Put event listeners into place
+    window.addEventListener("DOMContentLoaded", function() {
+            // Grab elements, create settings, etc.
+            var canvas = document.getElementById("canvas"),
+                    context = canvas.getContext("2d"),
+                    video = document.getElementById("video"),
+                    videoObj = { "video": true },
+                    errBack = function(error) {
+                            console.log("Video capture error: ", error.code); 
+                    };
+    
+            // Put video listeners into place
+            if(navigator.getUserMedia) { // Standard
+                    navigator.getUserMedia(videoObj, function(stream) {
+                            video.src = stream;
+                            video.play();
+                    }, errBack);
+            } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+                    navigator.webkitGetUserMedia(videoObj, function(stream){
+                            video.src = window.webkitURL.createObjectURL(stream);
+                            video.play();
+                    }, errBack);
+            }
+            else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+                    navigator.mozGetUserMedia(videoObj, function(stream){
+                            video.src = window.URL.createObjectURL(stream);
+                            video.play();
+                    }, errBack);
+            }
+    }, false);
+    
+    // Trigger photo take
+    $("snap").click(function() {
+            context.drawImage(video, 0, 0, 640, 480);
+    });
+    
+    
+    
+    
    
 });
 
